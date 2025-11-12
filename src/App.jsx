4933097fdd21054de8d0913f7795ea77192edf8d
Le Quoc Thai <22130250@st.hcmuaf.edu.vn>
2025-11-12 21:16:49 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 
 import './App.scss'
 import Header from './components/Header/Header.jsx'
-
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <div className='app'>
+      <div className='app-container'>
         <Header />
+        <div>
+          test link
+          <div>
+            <button>
+              <Link to="/users">go to user page</Link>
+            </button>
+            <button>
+              <Link to="/admins">go to admin page</Link>
+            </button>
+
+          </div>
+        </div>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import User from './components/User/User.jsx';
+import Admin from './components/Admin/Admin.jsx';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="users" element={<User />} />
+      <Route path="admins" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 
   // </StrictMode>,
 )
