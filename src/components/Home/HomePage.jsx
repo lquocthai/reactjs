bdca0cbd1d0fb083063447a195bc785432f050d6
Le Quoc Thai <22130250@st.hcmuaf.edu.vn>
2025-11-19 23:18:36 +0700

    video 54

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -8,6 +8,18 @@ const HomePage = () => {
                     src={videoHomepage}
                 />
             </video>
+            <div className='homepage-content'>
+                <div className='title-1'>
+                    Get 3.5x more data with a form expert
+                </div>
+                <div className='title-2'>
+                    Backed by over a decade of experience, Typeform AI helps you build expertly-designed, best-practice forms proven to get more responses.
+                </div>
+                <div className='title-3'>
+                    <button>Get's stared ,it's free</button>
+                </div>
+
+            </div>
         </div>
     )
 }
